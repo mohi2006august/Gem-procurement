842ba4a5bde7735109022a7b40094675f3de8f53
mohi2006august <nizamuddinnumaan822@gmail.com>
2026-09-07 12:22:02 +0530

    Fix 404 errors by adding base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,6 @@
 import { defineConfig } from 'astro/config';
-export default defineConfig({});
+
+export default defineConfig({
+  site: 'https://mohi2006august.github.io',
+  base: '/Gem-procurement',
+});
